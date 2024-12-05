259138f1a28cb6bf8e769d2929dc2fcdb1d3af5e
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-12-05 19:36:30 +0600

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CanceledPage from "./pages/CanceledPage";
+import CompletedPage from "./pages/CompletedPage";
+import CreatePage from "./pages/CreatePage";
+import DashboardPage from "./pages/DashboardPage";
+import NewPage from "./pages/NewPage";
+import NotFound from "./pages/NotFound";
+import ProfilePage from "./pages/ProfilePage";
+import ProgressPage from "./pages/ProgressPage";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<DashboardPage />} />
+        <Route exact path="/Create" element={<CreatePage />} />
+        <Route exact path="/All" element={<NewPage />} />
+        <Route exact path="/Progress" element={<ProgressPage />} />
+        <Route exact path="/Completed" element={<CompletedPage />} />
+        <Route exact path="/Canceled" element={<CanceledPage />} />
+        <Route exact path="/Profile" element={<ProfilePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
-
-//16
+export default App;
