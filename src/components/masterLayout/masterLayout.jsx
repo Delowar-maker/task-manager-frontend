e6e4c22b2ef08a9e5e00cdff16b198c9cd85d698
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-12-07 21:44:01 +0600

    images

--- src/components/masterLayout/masterLayout.jsx
+++ src/components/masterLayout/masterLayout.jsx
@@ -1,192 +1,48 @@
-import { useRef } from "react";
-import { Container, Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 import {
-  AiOutlineCheckCircle,
-  AiOutlineEdit,
   AiOutlineLogout,
   AiOutlineMenuUnfold,
   AiOutlineUser,
 } from "react-icons/ai";
-
-import { BsHourglass, BsListNested } from "react-icons/bs";
-import { MdOutlineCancelPresentation } from "react-icons/md";
-import { RiDashboardLine } from "react-icons/ri";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/images/logo.svg";
-import { getUserDetails, removeSessions } from "../../helper/SessionHelper";
 
-const MasterLayout = (props) => {
-  let contentRef,
-    sideNavRef = useRef();
-
-  const onLogout = () => {
-    removeSessions();
-  };
-
-  const MenuBarClickHandler = () => {
-    let sideNav = sideNavRef;
-    let content = contentRef;
-    if (sideNav.classList.contains("side-nav-open")) {
-      sideNav.classList.add("side-nav-close");
-      sideNav.classList.remove("side-nav-open");
-      content.classList.add("content-expand");
-      content.classList.remove("content");
-    } else {
-      sideNav.classList.remove("side-nav-close");
-      sideNav.classList.add("side-nav-open");
-      content.classList.remove("content-expand");
-      content.classList.add("content");
-    }
-  };
+const MasterLayout = () => {
   return (
-    <div>
-      <Navbar className="fixed-top px-0 shadow-sm ">
-        <Container fluid={true}>
-          <Navbar.Brand>
-            <a className="icon-nav m-0 h5" onClick={MenuBarClickHandler}>
-              <AiOutlineMenuUnfold />
-            </a>
-            <img className="nav-logo mx-2" src={logo} alt="logo" />
-          </Navbar.Brand>
-
-          <div className="float-right h-auto d-flex">
-            <div className="user-dropdown">
-              <img
-                className="icon-nav-img icon-nav"
-                src={getUserDetails()["photo"]}
-                alt="imags"
-              />
-              <div className="user-dropdown-content ">
-                <div className="mt-4 text-center">
-                  <img
-                    className="icon-nav-img"
-                    src={getUserDetails()["photo"]}
-                    alt=""
-                  />
-                  <h6>{getUserDetails()["firstName"]}</h6>
-                  <hr className="user-dropdown-divider  p-0" />
-                </div>
-                <NavLink to="/Profile" className="side-bar-item">
-                  <AiOutlineUser className="side-bar-item-icon" />
-                  <span className="side-bar-item-caption">Profile</span>
-                </NavLink>
-                <a onClick={onLogout} className="side-bar-item">
-                  <AiOutlineLogout className="side-bar-item-icon" />
-                  <span className="side-bar-item-caption">Logout</span>
-                </a>
+    <Navbar className="fixed-top px-0 shadow-sm">
+      <Container fluid={true}>
+        <Navbar.Brand className="d-flex">
+          <a className="icon-nav m-3 h5 ">
+            <AiOutlineMenuUnfold />
+          </a>
+          <h1>Task Manager</h1>
+        </Navbar.Brand>
+        <div className="float-right h-auto d-flex">
+          <div className="user-dropdown">
+            <img
+              className="icon-nav-img icon-nav"
+              src="holder.js/171x180"
+              alt=""
+            />
+            <div className="user-dropdown-content ">
+              <div className="mt-4 text-center">
+                <img className="icon-nav-img" src="holder.js/171x180" alt="" />
+                <h6>frist Name</h6>
+                <hr className="user-dropdown-divider  p-0" />
               </div>
+              <NavLink to="/Profile" className="side-bar-item">
+                <AiOutlineUser className="side-bar-item-icon" />
+                <span className="side-bar-item-caption">Profile</span>
+              </NavLink>
+              <a className="side-bar-item">
+                <AiOutlineLogout className="side-bar-item-icon" />
+                <span className="side-bar-item-caption">Logout</span>
+              </a>
             </div>
           </div>
-        </Container>
-      </Navbar>
-
-      <div
-        ref={(div) => {
-          sideNavRef = div;
-        }}
-        className="side-nav-open"
-      >
-        <NavLink
-          className={(navData) =>
-            navData.isActive
-              ? "side-bar-item-active side-bar-item mt-2"
-              : "side-bar-item mt-2"
-          }
-          to="/"
-          end
-        >
-          <RiDashboardLine className="side-bar-item-icon" />
-          <span className="side-bar-item-caption">Dashboard</span>
-        </NavLink>
-
-        <NavLink
-          className={(navData) =>
-            navData.isActive
-              ? "side-bar-item-active side-bar-item mt-2"
-              : "side-bar-item mt-2"
-          }
-          to="/Create"
-        >
-          <AiOutlineEdit className="side-bar-item-icon" />
-          <span className="side-bar-item-caption">Create New</span>
-        </NavLink>
-
-        <NavLink
-          className={(navData) =>
-            navData.isActive
-              ? "side-bar-item-active side-bar-item mt-2"
-              : "side-bar-item mt-2"
-          }
-          to="/All"
-        >
-          <BsListNested className="side-bar-item-icon" />
-          <span className="side-bar-item-caption">New Task</span>
-        </NavLink>
-
-        <NavLink
-          className={(navData) =>
-            navData.isActive
-              ? "side-bar-item-active side-bar-item mt-2"
-              : "side-bar-item mt-2"
-          }
-          to="/Progress"
-        >
-          <BsHourglass className="side-bar-item-icon" />
-          <span className="side-bar-item-caption">In Progress</span>
-        </NavLink>
-
-        <NavLink
-          className={(navData) =>
-            navData.isActive
-              ? "side-bar-item-active side-bar-item mt-2"
-              : "side-bar-item mt-2"
-          }
-          to="/Completed"
-        >
-          <AiOutlineCheckCircle className="side-bar-item-icon" />
-          <span className="side-bar-item-caption">Completed</span>
-        </NavLink>
-
-        <NavLink
-          className={(navData) =>
-            navData.isActive
-              ? "side-bar-item-active side-bar-item mt-2"
-              : "side-bar-item mt-2"
-          }
-          to="/Canceled"
-        >
-          <MdOutlineCancelPresentation className="side-bar-item-icon" />
-          <span className="side-bar-item-caption">Canceled</span>
-        </NavLink>
-      </div>
-
-      <div ref={(div) => (contentRef = div)} className="content">
-        {props.children}
-      </div>
-    </div>
-    // <Navbar expand="lg" className="bg-body-tertiary">
-    //   <Container>
-    //     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="me-auto">
-    //         <Nav.Link href="#home">Home</Nav.Link>
-    //         <Nav.Link href="#link">Link</Nav.Link>
-    //         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-    //           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-    //           <NavDropdown.Item href="#action/3.2">
-    //             Another action
-    //           </NavDropdown.Item>
-    //           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-    //           <NavDropdown.Divider />
-    //           <NavDropdown.Item href="#action/3.4">
-    //             Separated link
-    //           </NavDropdown.Item>
-    //         </NavDropdown>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
+        </div>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/css/animate.min.css";
 import "./assets/css/bootstrap.min.css";
-// import "./assets/css/style.css";
-// import "./index.css";
+import "./assets/css/dropdownmenu.css";
+import "./assets/css/progress.css";
+import "./assets/css/style.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
