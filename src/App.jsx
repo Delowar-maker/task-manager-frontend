5737917449704f531eb3c4a6c5078c7027cc7d35
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-12-12 18:40:51 +0600

    login if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "react-toastify/dist/ReactToastify.css";
+// import FullScreenLoader from "./components/masterLayout/FullScreenLoader.jsx";
+// import CanceledPage from "./pages/CanceledPage.jsx";
+// import CompletedPage from "./pages/CompletedPage.jsx";
+// import CreatePage from "./pages/CreatePage.jsx";
+// import DashboardPage from "./pages/DashboardPage.jsx";
+// import LoginPage from "./pages/LoginPage.jsx";
+// import NewPage from "./pages/NewPage.jsx";
+// import NotFoundPage from "./pages/NotFoundPage.jsx";
+// import ProfilePage from "./pages/ProfilePage.jsx";
+// import ProgressPage from "./pages/ProgressPage.jsx";
+
 import "react-toastify/dist/ReactToastify.css";
-import FullScreenLoader from "./components/masterLayout/FullScreenLoader.jsx";
 import CanceledPage from "./pages/CanceledPage.jsx";
 import CompletedPage from "./pages/CompletedPage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
@@ -10,27 +21,68 @@ import NewPage from "./pages/NewPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import ProgressPage from "./pages/ProgressPage.jsx";
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import FullscreenLoader from "./components/masterLayout/FullScreenLoader.jsx";
+import { getToken } from "./helper/SessionHelper";
 import RegistrationPage from "./pages/RegistrationPage.jsx";
+
+// import RegistrationPage from "./pages/RegistrationPage.jsx";
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route exact path="/Create" element={<CreatePage />} />
-          <Route exact path="/All" element={<NewPage />} />
-          <Route exact path="/Progress" element={<ProgressPage />} />
-          <Route exact path="/Completed" element={<CompletedPage />} />
-          <Route exact path="/Canceled" element={<CanceledPage />} />
-          <Route exact path="/Profile" element={<ProfilePage />} />
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/Registration" element={<RegistrationPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-      <FullScreenLoader />
-    </>
-  );
+  // return (
+  //   <>
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route path="/" element={<DashboardPage />} />
+  //         <Route exact path="/Create" element={<CreatePage />} />
+  //         <Route exact path="/All" element={<NewPage />} />
+  //         <Route exact path="/Progress" element={<ProgressPage />} />
+  //         <Route exact path="/Completed" element={<CompletedPage />} />
+  //         <Route exact path="/Canceled" element={<CanceledPage />} />
+  //         <Route exact path="/Profile" element={<ProfilePage />} />
+  //         <Route exact path="/login" element={<LoginPage />} />
+  //         <Route exact path="/Registration" element={<RegistrationPage />} />
+  //         <Route path="*" element={<NotFoundPage />} />
+  //       </Routes>
+  //     </BrowserRouter>
+  //     <FullScreenLoader />
+  //   </>
+  // );
+  if (getToken) {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<DashboardPage />} />
+            <Route exact path="/Create" element={<CreatePage />} />
+            <Route exact path="/All" element={<NewPage />} />
+            <Route exact path="/Progress" element={<ProgressPage />} />
+            <Route exact path="/Completed" element={<CompletedPage />} />
+            <Route exact path="/Canceled" element={<CanceledPage />} />
+            <Route exact path="/Profile" element={<ProfilePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
+        <FullscreenLoader />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/Login" replace />} />
+            <Route exact path="/Login" element={<LoginPage />} />
+            <Route exact path="/Registration" element={<RegistrationPage />} />
+            {/* <Route exact path="/SendOTP" element={<SendOTPPage />} />
+            <Route exact path="/VerifyOTP" element={<VerifyOTPPage />} /> */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
+        <FullscreenLoader />
+      </>
+    );
+  }
 };
 
 export default App;
