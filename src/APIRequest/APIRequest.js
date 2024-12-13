8d80edd9934f534ac350fdd93670548e142d632a
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-12-13 17:52:35 +0600

    new task backend

--- src/APIRequest/APIRequest.js
+++ src/APIRequest/APIRequest.js
@@ -3,54 +3,47 @@
 
 import axios from "axios";
 import { ErrorToast, SuccessToast } from "../helper/FormHelper";
-import { setToken, setUserDetails } from "../helper/SessionHelper";
+import { getToken, setToken, setUserDetails } from "../helper/SessionHelper";
 import { HideLoader, ShowLoader } from "../redux/state-slice/settings-slice";
 import store from "../redux/store/store";
 
+
+
 const BaseURL = "https://task-manager-jade-seven.vercel.app/api/v1";
 
-export function RegistrationRequest(email, firstName, lastName, mobile, password, photo) {
-    store.dispatch(ShowLoader());
-    let URL = BaseURL + "/registration";
-    let PostBody = { email: email, firstName: firstName, lastName: lastName, mobile: mobile, password: password, photo: photo }
+const AxiosHeader = { headers: { "token-key": getToken() } }
 
-    return axios.post(URL, PostBody)
-        .then((res) => {
-            store.dispatch(HideLoader());
-            if (res.status === 200) {
-                if (res.data['status'] === "fail") {
-                    if (res.data['data']['keyPattern']['email'] === 1) {
-                        ErrorToast("Email Already Exist")
-                        return false;
-                    }
-                    else {
-                        ErrorToast("Something Went Wrong")
-                        return false;
-                    }
-                }
-                else {
-                    SuccessToast("Registration Success")
-                    return true;
-                }
-            }
-            else {
-                ErrorToast("Something Went Wrong")
-                return false;
-            }
-        })
-        .catch((error) => {
-            store.dispatch(HideLoader());
-            ErrorToast(error);
-            return false
-        });
+export function NewTaskRequest(title, description) {
 
+
+    store.dispatch(ShowLoader())
+
+    let URL = BaseURL + "/createTask";
+    let PostBody = { "title": title, "description": description, status: "New" }
+
+    return axios.post(URL, PostBody, AxiosHeader).then((res) => {
+        store.dispatch(HideLoader())
+        if (res.status === 200) {
+            SuccessToast("New Task Created")
+            return true;
+        }
+        else {
+            ErrorToast("Something Went Wrong")
+            return false;
+        }
+
+    }).catch((err) => {
+        ErrorToast(`${err}"Something Went Wrong"`)
+        store.dispatch(HideLoader())
+        return false;
+    })
 }
 
 
 export function LoginRequest(email, password) {
     store.dispatch(ShowLoader())
     let URL = BaseURL + "/login";
-    let PostBody = { email: email, password: password }
+    let PostBody = { "email": email, "password": password }
     return axios.post(URL, PostBody).then((res) => {
         store.dispatch(HideLoader())
         if (res.status === 200) {
@@ -64,8 +57,41 @@ export function LoginRequest(email, password) {
             return false;
         }
     }).catch((err) => {
-        ErrorToast(err)
+        ErrorToast(`${err}"Something Went Wrong"`)
         store.dispatch(HideLoader())
         return false;
     });
+}
+
+export function RegistrationRequest(email, firstName, lastName, mobile, password, photo) {
+    store.dispatch(ShowLoader())
+    let URL = BaseURL + "/registration";
+    let PostBody = { email: email, firstName: firstName, lastName: lastName, mobile: mobile, password: password, photo: photo }
+    return axios.post(URL, PostBody).then((res) => {
+        store.dispatch(HideLoader())
+        if (res.status === 200) {
+            if (res.data['status'] === "fail") {
+                if (res.data['data']['keyPattern']['email'] === 1) {
+                    ErrorToast("Email Already Exist")
+                    return false;
+                }
+                else {
+                    ErrorToast("Something Went Wrong")
+                    return false;
+                }
+            }
+            else {
+                SuccessToast("Registration Success")
+                return true;
+            }
+        }
+        else {
+            ErrorToast("Something Went Wrong")
+            return false;
+        }
+    }).catch((err) => {
+        store.dispatch(HideLoader())
+        ErrorToast(`${err}"Something Went Wrong"`)
+        return false;
+    })
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import "react-toastify/dist/ReactToastify.css";
-// import FullScreenLoader from "./components/masterLayout/FullScreenLoader.jsx";
 // import CanceledPage from "./pages/CanceledPage.jsx";
 // import CompletedPage from "./pages/CompletedPage.jsx";
 // import CreatePage from "./pages/CreatePage.jsx";
@@ -11,7 +9,38 @@
 // import ProfilePage from "./pages/ProfilePage.jsx";
 // import ProgressPage from "./pages/ProgressPage.jsx";
 
-import "react-toastify/dist/ReactToastify.css";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import FullscreenLoader from "./components/masterLayout/FullScreenLoader.jsx";
+// import RegistrationPage from "./pages/RegistrationPage.jsx";
+
+// const App = () => {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<DashboardPage />} />
+//           <Route exact path="/Create" element={<CreatePage />} />
+//           <Route exact path="/All" element={<NewPage />} />
+//           <Route exact path="/Progress" element={<ProgressPage />} />
+//           <Route exact path="/Completed" element={<CompletedPage />} />
+//           <Route exact path="/Canceled" element={<CanceledPage />} />
+//           <Route exact path="/Profile" element={<ProfilePage />} />
+//           <Route exact path="/login" element={<LoginPage />} />
+//           <Route exact path="/Registration" element={<RegistrationPage />} />
+//           <Route path="*" element={<NotFoundPage />} />
+//         </Routes>
+//       </BrowserRouter>
+//       <FullscreenLoader />
+//     </>
+//   );
+// };
+
+// export default App;
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import FullscreenLoader from "./components/masterLayout/FullScreenLoader.jsx";
+import { getToken } from "./helper/SessionHelper";
 import CanceledPage from "./pages/CanceledPage.jsx";
 import CompletedPage from "./pages/CompletedPage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
@@ -21,34 +50,10 @@ import NewPage from "./pages/NewPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import ProgressPage from "./pages/ProgressPage.jsx";
-
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import FullscreenLoader from "./components/masterLayout/FullScreenLoader.jsx";
-import { getToken } from "./helper/SessionHelper";
 import RegistrationPage from "./pages/RegistrationPage.jsx";
 
-// import RegistrationPage from "./pages/RegistrationPage.jsx";
 const App = () => {
-  // return (
-  //   <>
-  //     <BrowserRouter>
-  //       <Routes>
-  //         <Route path="/" element={<DashboardPage />} />
-  //         <Route exact path="/Create" element={<CreatePage />} />
-  //         <Route exact path="/All" element={<NewPage />} />
-  //         <Route exact path="/Progress" element={<ProgressPage />} />
-  //         <Route exact path="/Completed" element={<CompletedPage />} />
-  //         <Route exact path="/Canceled" element={<CanceledPage />} />
-  //         <Route exact path="/Profile" element={<ProfilePage />} />
-  //         <Route exact path="/login" element={<LoginPage />} />
-  //         <Route exact path="/Registration" element={<RegistrationPage />} />
-  //         <Route path="*" element={<NotFoundPage />} />
-  //       </Routes>
-  //     </BrowserRouter>
-  //     <FullScreenLoader />
-  //   </>
-  // );
-  if (getToken) {
+  if (getToken()) {
     return (
       <>
         <BrowserRouter>
@@ -74,8 +79,7 @@ const App = () => {
             <Route path="/" element={<Navigate to="/Login" replace />} />
             <Route exact path="/Login" element={<LoginPage />} />
             <Route exact path="/Registration" element={<RegistrationPage />} />
-            {/* <Route exact path="/SendOTP" element={<SendOTPPage />} />
-            <Route exact path="/VerifyOTP" element={<VerifyOTPPage />} /> */}
+
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </BrowserRouter>
