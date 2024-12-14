b266b7ec1b2bddacea2eb05563c19004bdce7a51
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-12-14 16:29:07 +0600

    task redux using

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import settingsReducer from "../state-slice/settings-slice";
+import taskReducer from "../state-slice/task-slice";
 
 
 export default configureStore({
     reducer: {
         settings: settingsReducer,
+        task: taskReducer,
     },
 })
